f76b4281d7721977cc98d70fe07776fac9fabe70
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-14 17:45:49 -0300

    Create request service

--- src/services/request.js
+++ src/services/request.js
@@ -1,5 +1,21 @@
-export default async function request(url) {
-  const response = await fetch(url)
+export default async function request(url, body) {
+  const requestOptions = {
+    method: 'POST',
+    body: body,
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }),
+  }
+  const response = await fetch(url, requestOptions)
 
-  return response.ok ? response.json() : response.text()
+  const data = await response.text()
+
+  const parsedResponse = {
+    status: response.status,
+    data:
+      data.length > 0 && response.headers.get('content-type')?.includes('application/json') ? JSON.parse(data) : data,
+  }
+
+  return parsedResponse
 }
